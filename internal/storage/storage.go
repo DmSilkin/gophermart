1d11fbe4640dbf30cb29e0b622b851800663df7d
Dmitry Silkin <chief032@mail.ru>
2023-04-23 14:56:49 +0300

    cookie login

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -88,7 +88,7 @@ func NewDBController(dsn string, logger zerolog.Logger) (*DBController, error) {
 	}
 
 	m, err := migrate.NewWithDatabaseInstance(
-		"file://../gophermart/migrations",
+		"file://cmd/gophermart/migrations",
 		"pgx", driver)
 
 	if err != nil {
